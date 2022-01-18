a989c9d36c6355c48ee0665a61108088cdf37d97
Asher <asherzengel@gmail.com>
2022-01-18 23:02:14 +0200

    added tests and refactored code

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -10,84 +10,31 @@ class Shop {
   constructor(items=[]){
     this.items = items;
   }
-  updateQuality() {
-    for (var i = 0; i < this.items.length; i++) {
-      const splitName = this.items[i].name.split(' ');
-      if (!splitName.includes("Aged", "Brie") && !splitName.includes("Backstage", "passes", "to", "a", "TAFKAL80ETC", "concert")) {
-        if (!splitName.includes("Sulfuras", "Hand", "of", "Ragnaros")) {
-          if (splitName.includes("Conjured")) {
-            if (this.items[i].quality > 0) {
-              this.items[i].quality = this.items[i].quality - 1;
-            }
-          }
-          if (this.items[i].quality > 0) {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (splitName.includes("Conjured")) {
-            if (this.items[i].quality < 50) {
-              this.items[i].quality = this.items[i].quality + 1;
-            }
-          }
-          if (splitName.includes("Backstage", "passes", "to", "a", "TAFKAL80ETC", "concert")) {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-              if (splitName.includes("Conjured")) {
-                if (this.items[i].quality < 50) {
-                  this.items[i].quality = this.items[i].quality + 1;
-                }
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-              if (splitName.includes("Conjured")) {
-                if (this.items[i].quality < 50) {
-                  this.items[i].quality = this.items[i].quality + 1;
-                }
-              }
-            }
-          }
-        }
-      }
-      if (!splitName.includes("Sulfuras", "Hand", "of", "Ragnaros")) {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (!splitName.includes("Aged", "Brie")) {
-          if (!splitName.includes("Backstage", "passes", "to", "a", "TAFKAL80ETC", "concert")) {
-            if (this.items[i].quality > 0) {
-              if (!splitName.includes("Sulfuras", "Hand", "of", "Ragnaros")) {
-                this.items[i].quality = this.items[i].quality - 1;
-                if (splitName.includes("Conjured")) {
-                  if (this.items[i].quality > 0) {
-                    this.items[i].quality = this.items[i].quality - 1;
-                  }
-                }
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-            if (splitName.includes("Conjured")) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
+
+  updateQuality(item) {
+    if (item.name.includes("Aged Brie")) {
+      item.quality += item.sellIn > 0 ? 1 : 2;
+    } else if (item.name.includes("Backstage passes")) {
+      if (item.sellIn > 10) item.quality += 1;
+      if (item.sellIn <= 10 && item.sellIn > 5) item.quality += 2;
+      if (item.sellIn <= 5) item.quality += 3;
+      if (item.sellIn < 0) item.quality = 0;
+    } else {
+      item.quality -= item.sellIn > 0 ? 1 : 2;
+    }
+    if (item.quality < 0) item.quality = 0;
+    if (item.quality > 50) item.quality = 50;
+  }
+
+  endOfDay() {
+    for (const item of this.items) {
+      if (!item.name.includes("Sulfuras, Hand of Ragnaros")) {
+        item.sellIn -= 1;
+
+        this.updateQuality(item);
+        if (item.name.includes("Conjured")) this.updateQuality(item);
       }
     }
-
     return this.items;
   }
 }
--- test/test_gilded_rose.js
+++ test/test_gilded_rose.js
@@ -1,11 +1,165 @@
-var {expect} = require('chai');
-var {Shop, Item} = require('../src/gilded_rose.js');
-describe("Gilded Rose", function() {
+const {assert} = require('chai');
+const {Shop, Item} = require('../src/gilded_rose.js');
+describe("Gilded Rose", () => {
+ 
+  it('should decrease sellIn value and increase the quality of Aged Brie by 1 if sellIn date has not yet passed', () => {
+    const gildedRose = new Shop([new Item("Aged Brie", 10, 29)]);
+    const items = gildedRose.endOfDay();
+    assert.equal(items[0].name, "Aged Brie");
+    assert.equal(items[0].sellIn, 9);
+    assert.equal(items[0].quality, 30);
+  });
 
-  it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).to.equal("fixme");
+it('should decrease sellIn by one and decrease quality by 2 for Aged Brie once the sellIn date has passed', () => {
+    const gildedRose = new Shop([new Item("Aged Brie", -2, 41)]);
+    const items = gildedRose.endOfDay();
+    assert.equal(items[0].name, "Aged Brie");
+    assert.equal(items[0].sellIn, -3);
+    assert.equal(items[0].quality, 43);
+  });
+
+  it('should not increase the quality of Aged Brie past 50 even when sellIn date has passed', () => {
+    const gildedRose = new Shop([new Item("Aged Brie", -10, 49)]);
+    const items = gildedRose.endOfDay();
+    assert.equal(items[0].name, "Aged Brie");
+    assert.equal(items[0].sellIn, -11);
+    assert.equal(items[0].quality, 50);
+  });
+
+  it('should increase quality of Conjured Aged Brie by 2 if the sellIn date has not yet passed', () => {
+    const gildedRose = new Shop([new Item("Conjured Aged Brie", 5, 37)]);
+    const items = gildedRose.endOfDay();
+    assert.equal(items[0].name, "Conjured Aged Brie");
+    assert.equal(items[0].sellIn, 4);
+    assert.equal(items[0].quality, 39);
+  });
+
+  it('should increase quality of Conjured Aged Brie by 4 if the sellIn date has passed', () => {
+    const gildedRose = new Shop([new Item("Conjured Aged Brie", -5, 37)]);
+    const items = gildedRose.endOfDay();
+    assert.equal(items[0].name, "Conjured Aged Brie");
+    assert.equal(items[0].sellIn, -6);
+    assert.equal(items[0].quality, 41);
+  });
+
+it('should increase the quality of Backstage Passes by 1 if there are more than 10 days to the concert', () => {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 15, 8)]);
+    const items = gildedRose.endOfDay();
+    assert.equal(items[0].name, "Backstage passes to a TAFKAL80ETC concert");
+    assert.equal(items[0].sellIn, 14);
+    assert.equal(items[0].quality, 9);
+  });
+
+  it('should increase the quality of Backstage Passes by 2 if there are 10 days or less to the concert', () => {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 10, 25)]);
+    const items = gildedRose.endOfDay();
+    assert.equal(items[0].name, "Backstage passes to a TAFKAL80ETC concert");
+    assert.equal(items[0].sellIn, 9);
+    assert.equal(items[0].quality, 27);
+  });
+
+  it('should increase the quality of Backstage Passes by 3 if there are less than 5 days to the concert', () => {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 4, 39)]);
+    const items = gildedRose.endOfDay();
+    assert.equal(items[0].name, "Backstage passes to a TAFKAL80ETC concert");
+    assert.equal(items[0].sellIn, 3);
+    assert.equal(items[0].quality, 42);
+  });
+
+  it('quality of Backstage Passes should drop to 0 after the concert has passed', () => {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 0, 45)]);
+    const items = gildedRose.endOfDay();
+    assert.equal(items[0].name, "Backstage passes to a TAFKAL80ETC concert");
+    assert.equal(items[0].sellIn, -1);
+    assert.equal(items[0].quality, 0);
+  });
+
+  it('should increase the quality of Conjured Backstage Passes by 6 if there are less than 5 days to the concert', () => {
+    const gildedRose = new Shop([new Item("Conjured Backstage passes to a TAFKAL80ETC concert", 4, 39)]);
+    const items = gildedRose.endOfDay();
+    assert.equal(items[0].name, "Conjured Backstage passes to a TAFKAL80ETC concert");
+    assert.equal(items[0].sellIn, 3);
+    assert.equal(items[0].quality, 45);
+  });
+
+  it('should increase the quality of Conjured Backstage Passes by 4 if there are 10 days or less to the concert', () => {
+    const gildedRose = new Shop([new Item("Conjured Backstage passes to a TAFKAL80ETC concert", 9, 39)]);
+    const items = gildedRose.endOfDay();
+    assert.equal(items[0].name, "Conjured Backstage passes to a TAFKAL80ETC concert");
+    assert.equal(items[0].sellIn, 8);
+    assert.equal(items[0].quality, 43);
+  });
+  
+  it('should increase the quality of Conjured Backstage Passes by 2 if there are more than 10 days to the concert', () => {
+    const gildedRose = new Shop([new Item("Conjured Backstage passes to a TAFKAL80ETC concert", 13, 39)]);
+    const items = gildedRose.endOfDay();
+    assert.equal(items[0].name, "Conjured Backstage passes to a TAFKAL80ETC concert");
+    assert.equal(items[0].sellIn, 12);
+    assert.equal(items[0].quality, 41);
+  });
+
+  it('quality of Conjured Backstage Passes should drop to 0 after the concert has passed', () => {
+    const gildedRose = new Shop([new Item("Conjured Backstage passes to a TAFKAL80ETC concert", 0, 45)]);
+    const items = gildedRose.endOfDay();
+    assert.equal(items[0].name, "Conjured Backstage passes to a TAFKAL80ETC concert");
+    assert.equal(items[0].sellIn, -1);
+    assert.equal(items[0].quality, 0);
+  });
+  
+  it('should decrease sellIn value and quality of Pickles by 1', () => {
+    const gildedRose = new Shop([new Item("Pickles", 10, 29)]);
+    const items = gildedRose.endOfDay();
+    assert.equal(items[0].name, "Pickles");
+    assert.equal(items[0].sellIn, 9);
+    assert.equal(items[0].quality, 28);
+  });
+
+  it('should decrease sellIn value and decrease quality of Pickles by 2 if sellIn date has passed', () => {
+    const gildedRose = new Shop([new Item("Pickles", -10, 9)]);
+    const items = gildedRose.endOfDay();
+    assert.equal(items[0].name, "Pickles");
+    assert.equal(items[0].sellIn, -11);
+    assert.equal(items[0].quality, 7);
+  });
+
+  it('should decrease sellIn value and not decrease quality of Pickles below 0 even if sellIn date has passed', () => {
+    const gildedRose = new Shop([new Item("Pickles", -10, 1)]);
+    const items = gildedRose.endOfDay();
+    assert.equal(items[0].name, "Pickles");
+    assert.equal(items[0].sellIn, -11);
+    assert.equal(items[0].quality, 0);
+  });
+
+  it('should decrease sellIn value and decrease quality of Conjured Pickles by 2', () => {
+    const gildedRose = new Shop([new Item("Conjured Pickles", 10, 29)]);
+    const items = gildedRose.endOfDay();
+    assert.equal(items[0].name, "Conjured Pickles");
+    assert.equal(items[0].sellIn, 9);
+    assert.equal(items[0].quality, 27);
+  });
+  
+  it('should decrease sellIn value and decrease quality of Conjured Pickles by 4 if sellIn date has passed', () => {
+    const gildedRose = new Shop([new Item("Conjured Pickles", -7, 8)]);
+    const items = gildedRose.endOfDay();
+    assert.equal(items[0].name, "Conjured Pickles");
+    assert.equal(items[0].sellIn, -8);
+    assert.equal(items[0].quality, 4);
+  });
+  
+  it('should maintain sellIn and quality values for Sulfuras', () => {
+    const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 0, 80)]);
+    const items = gildedRose.endOfDay();
+    assert.equal(items[0].name, "Sulfuras, Hand of Ragnaros");
+    assert.equal(items[0].sellIn, 0);
+    assert.equal(items[0].quality, 80);
+  });
+
+  it('should maintain sellIn and quality values for Conjured Sulfuras', () => {
+    const gildedRose = new Shop([new Item("Conjured Sulfuras, Hand of Ragnaros", 0, 80)]);
+    const items = gildedRose.endOfDay();
+    assert.equal(items[0].name, "Conjured Sulfuras, Hand of Ragnaros");
+    assert.equal(items[0].sellIn, 0);
+    assert.equal(items[0].quality, 80);
   });
 
 });
