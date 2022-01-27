149fd4289db5bde75572fdcf9288e92a3162cea9
Asher <asherzengel@gmail.com>
2022-01-27 16:28:42 +0200

    cm interview solution

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -7,35 +7,51 @@ class Item {
 }
 
 class Shop {
-  constructor(items=[]){
+  constructor(items = []) {
     this.items = items;
   }
 
   updateQuality() {
     for (const item of this.items) {
-      if (!item.name.includes("Sulfuras, Hand of Ragnaros")) {
-        item.sellIn -= 1;
 
-        this.updateItemQuality(item);
-        if (item.name.includes("Conjured")) this.updateItemQuality(item);
-      }
+      this.updateItem(item);
+      // if (item.name.includes("Conjured")) this.updateItemQuality(item);
     }
     return this.items;
   }
 
-  updateItemQuality(item) {
-    if (item.name.includes("Aged Brie")) {
-      item.quality += item.sellIn >= 0 ? 1 : 2;
-    } else if (item.name.includes("Backstage passes")) {
-      if (item.sellIn > 10) item.quality += 1;
-      if (item.sellIn <= 10 && item.sellIn > 5) item.quality += 2;
-      if (item.sellIn <= 5) item.quality += 3;
-      if (item.sellIn < 0) item.quality = 0;
-    } else {
-      item.quality -= item.sellIn >= 0 ? 1 : 2;
+  updateItem(item) {
+    if (item.name.includes("Sulfuras, Hand of Ragnaros")) {
+      return;
     }
+    item.sellIn -= 1;
+    const qualityFactor = this.getQualityFactor(item);
+    let difference = 0;
+    if (item.name.includes("Aged Brie")) {
+      difference = item.sellIn >= 0 ? 1 : 2;
+    } else if (item.name.includes("Backstage passes")) {
+      if (item.sellIn > 10) {
+        difference = 1;
+      } else if (item.sellIn > 5) {
+        difference = 2;
+      } else if (item.sellIn >= 0) {
+        difference = 3;
+      } else {
+        item.quality = 0;
+      }
+    } else {
+      difference = item.sellIn >= 0 ? -1 : -2;
+    }
+    item.quality += difference * qualityFactor;
     if (item.quality < 0) item.quality = 0;
     if (item.quality > 50) item.quality = 50;
+  }
+  getQualityFactor(item) {
+    let qualityFactor = 1;
+    if (item.name.includes("Conjured")) {
+      qualityFactor *= 2;
+    }
+    return qualityFactor;
   }
 }
 module.exports = {
